6abf7229c5e61c44f2c35ce08c5c4c525e6a83b2
JohnfyBUPT <zhfy888@163.com>
2016-05-11 20:58:25 +0800

    integrated loading from history feature

--- src/com/johnfrey/bupt/Bayesian.java
+++ src/com/johnfrey/bupt/Bayesian.java
@@ -15,6 +15,7 @@ public class Bayesian {
 	private static double pAbusive;
 	private static List<Double> p1Vec;
 	private static List<Double> p0Vec;
+	private static boolean hasHistory;
 	/**
 	 * @param args
 	 */
@@ -22,30 +23,9 @@ public class Bayesian {
 		init();
 		getTrainData();
 		getVocab();
-//		FileOutputStream fos = null;
-//		try {
-//			fos = new FileOutputStream("./dict.txt");
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
-//		OutputStreamWriter osw = new OutputStreamWriter(fos);
-//		BufferedWriter bw = new BufferedWriter(osw);
-//		for(int i = 0;i<vocabulary.size();i++){
-//			try {
-//				bw.write(vocabulary.get(i));
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		try {
-//			bw.flush();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		for(int i = 0;i<trainData.size();i++){
-			data2Vector(vocabulary, trainData.get(i));
-		}
-		train(trainDataClass);
+		data2Vector();
+		train();
+		writeDictnFeature();
 		List<String> test = new ArrayList<>();
 		List<Integer> testVec = new ArrayList<>();
 		test.add("sad");
@@ -53,11 +33,12 @@ public class Bayesian {
 		test.add("friend");
 		test.add("is");
 		test.add("gone");
-		testVec = test2Vector(vocabulary, test);
+		testVec = test2Vector(test);
 		judge(testVec);
 		System.out.println(test);
 	}
 
+	//初始化
 	public static void init() {
 		trainData = new ArrayList<>();
 		trainDataClass = new ArrayList<>();
@@ -65,18 +46,27 @@ public class Bayesian {
 		trainVector = new ArrayList<>();
 		p1Vec = new ArrayList<>();
 		p0Vec = new ArrayList<>();
+		hasHistory = false;
 	}
 
-	public void addEntry(int dataClass,String... data){
-		trainDataClass.add(dataClass);
-		List<String> mdata = new ArrayList<String>();
-		for(String word:data){
-			mdata.add(word);
-		}
-		trainData.add(mdata);
-	}
-
+	//从文本中形成固定格式的训练集
 	public static void getTrainData(){
+		//先查看字典是否已经生成,若已生成，跳过生成训练集的部分
+		File dict = new File("./dict.txt");
+		try{
+			FileInputStream in = new FileInputStream(dict);
+			InputStreamReader inputreader = new InputStreamReader(in);
+			BufferedReader buffreader = new BufferedReader(inputreader);
+			if(buffreader.readLine() != null){
+				hasHistory = true;
+				return;
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//如果字典还是空的，从训练文本生成训练集
 		File trainSet = new File("./data_temp.txt");
 		try {
 			FileInputStream in = new FileInputStream(trainSet);
@@ -121,85 +111,268 @@ public class Bayesian {
 //		trainDataClass.add(0);
 //		trainDataClass.add(1);
 	}
+
+	//从训练集形成字典
 	public static void getVocab(){
-		for(List<String> data:trainData){
-			for(String word:data){
-				if(!vocabulary.contains(word)){
+		//如果字典文件不为空，直接从文件形成字典
+		if(hasHistory){
+			System.out.println("has history!");
+			//TODO:获取存取的历史字典
+			File dict = new File("./dict.txt");
+			try{
+				FileInputStream in = new FileInputStream(dict);
+				InputStreamReader inputreader = new InputStreamReader(in);
+				BufferedReader buffreader = new BufferedReader(inputreader);
+				String word;
+				while ((word = buffreader.readLine()) != null){
 					vocabulary.add(word);
 				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
+			System.out.println(vocabulary);
 		}
-	}
-
-	public static void data2Vector(List<String> vocab, List<String> data) {
-		List<Integer> vec = new ArrayList<>();
-		int[] temp = new int[vocab.size()];
-		for (int i = 0; i < vocab.size(); i++) {
-			vec.add(0);
-		}
-		for (int i = 0; i < data.size(); i++) {
-			if (vocab.contains(data.get(i))) {
-				temp[(vocab.indexOf(data.get(i)))]++;
-			}
-		}
-		for (int i = 0; i < vocab.size(); i++) {
-			vec.set(i, temp[i]);
-		}
-		trainVector.add(vec);
-	}
-
-	public static void train(List<Integer> trainDataClass) {
-		int trainDocNum = trainVector.size();
-		int wordsNum = trainVector.get(0).size();
-		int count = 0;
-		for(int i = 0;i<trainDataClass.size();i++){
-			if(trainDataClass.get(i) == 1){
-				count++;
-			}
-		}
-		pAbusive = (double)(count)/trainDocNum;
-//		pAbusive = (double) (trainDataClass.stream().filter(dataClass -> dataClass.equals(1)).count()) / trainDocNum;
-		int[] p1Num = new int[wordsNum];
-		laplace(p1Num, 1);
-		int[] p0Num = new int[wordsNum];
-		laplace(p0Num, 1);
-		int p1Den = 2;
-		int p0Den = 2;
-		for (int i = 0; i < trainDocNum; i++) {
-			if (trainDataClass.get(i) == 1) {
-				for (int j = 0; j < wordsNum; j++) {
-					p1Num[j] += trainVector.get(i).get(j);
-				}
-				int countp1 = 0;
-				for(int j = 0;j<trainVector.get(i).size();j++){
-					if(trainVector.get(i).get(j) == 1){
-						countp1++;
+		//字典文件为空，从训练集生成字典
+		else {
+			for (List<String> data : trainData) {
+				for (String word : data) {
+					if (!vocabulary.contains(word)) {
+						vocabulary.add(word);
 					}
 				}
-				p1Den += countp1;
+			}
+		}
+	}
+
+	//将列表格式的训练集转换为向量
+	public static void data2Vector() {
+		if(hasHistory){
+			//有历史则不再计算向量
+		}
+		else {
+			for (int j = 0; j < trainData.size(); j++) {
+				List<Integer> vec = new ArrayList<>();
+				int[] temp = new int[vocabulary.size()];
+				for (int i = 0; i < vocabulary.size(); i++) {
+					vec.add(0);
+				}
+				for (int k = 0; k <vocabulary.size(); k++){
+					temp[k] = 0;
+				}
+				for (int i = 0; i < trainData.get(j).size(); i++) {
+					if (vocabulary.contains(trainData.get(j).get(i))) {
+						temp[(vocabulary.indexOf(trainData.get(j).get(i)))]++;
+					}
+				}
+				for (int i = 0; i < vocabulary.size(); i++) {
+					vec.set(i, temp[i]);
+				}
+				trainVector.add(vec);
+			}
+		}
+	}
+
+	//训练，由向量获取特征向量
+	public static void train() {
+		if(hasHistory){
+			//TODO：获取p0Vec.txt/p1Vec.txt中的特征值
+			File feat = new File("./p0Vec.txt");
+			try{
+				FileInputStream in = new FileInputStream(feat);
+				InputStreamReader inputreader = new InputStreamReader(in);
+				BufferedReader buffreader = new BufferedReader(inputreader);
+				String line;
+				while((line = buffreader.readLine()) != null){
+					String temp[] = line.split("\t");
+					for(int i = 0;i<temp.length;i++){
+						p0Vec.add(Double.parseDouble(temp[i]));
+					}
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			feat = new File("./p1Vec.txt");
+			try{
+				FileInputStream in = new FileInputStream(feat);
+				InputStreamReader inputreader = new InputStreamReader(in);
+				BufferedReader buffreader = new BufferedReader(inputreader);
+				String line;
+				while((line = buffreader.readLine()) != null){
+					String temp[] = line.split("\t");
+					for(int i = 0;i<temp.length;i++){
+						p1Vec.add(Double.parseDouble(temp[i]));
+					}
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			feat = new File("./pAbusive.txt");
+			try{
+				FileInputStream in = new FileInputStream(feat);
+				InputStreamReader inputreader = new InputStreamReader(in);
+				BufferedReader buffreader = new BufferedReader(inputreader);
+				String line;
+				if((line = buffreader.readLine()) != null){
+					pAbusive = Double.parseDouble(line);
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		//没有历史特征向量，开始计算特征向量
+		else {
+			int trainDocNum = trainVector.size();
+			int wordsNum = trainVector.get(0).size();
+			int count = 0;
+			for (int i = 0; i < trainDataClass.size(); i++) {
+				if (trainDataClass.get(i) == 1) {
+					count++;
+				}
+			}
+			pAbusive = (double) (count) / trainDocNum;
+//		pAbusive = (double) (trainDataClass.stream().filter(dataClass -> dataClass.equals(1)).count()) / trainDocNum;
+			int[] p1Num = new int[wordsNum];
+			laplace(p1Num, 1);
+			int[] p0Num = new int[wordsNum];
+			laplace(p0Num, 1);
+			int p1Den = 2;
+			int p0Den = 2;
+			for (int i = 0; i < trainDocNum; i++) {
+				if (trainDataClass.get(i) == 1) {
+					for (int j = 0; j < wordsNum; j++) {
+						p1Num[j] += trainVector.get(i).get(j);
+					}
+					int countp1 = 0;
+					for (int j = 0; j < trainVector.get(i).size(); j++) {
+						if (trainVector.get(i).get(j) == 1) {
+							countp1++;
+						}
+					}
+					p1Den += countp1;
 //				p1Den += trainVector.get(i).stream().filter(isThereWord -> isThereWord.equals(1)).count();
 
-			} else {
-				for (int k = 0; k < wordsNum; k++) {
-					p0Num[k] += trainVector.get(i).get(k);
-				}
-				int countp2 = 0;
-				for(int k = 0;k<trainVector.get(i).size();k++){
-					if(trainVector.get(i).get(k) == 1){
-						countp2++;
+				} else {
+					for (int k = 0; k < wordsNum; k++) {
+						p0Num[k] += trainVector.get(i).get(k);
 					}
-				}
-				p0Den += countp2;
+					int countp2 = 0;
+					for (int k = 0; k < trainVector.get(i).size(); k++) {
+						if (trainVector.get(i).get(k) == 1) {
+							countp2++;
+						}
+					}
+					p0Den += countp2;
 //				p0Den += trainVector.get(i).stream().filter(isThereWord -> isThereWord.equals(1)).count();
+				}
 			}
-		}
-		for (int m = 0; m < wordsNum; m++) {
-			p1Vec.add(Math.log((double) (p1Num[m]) / p1Den));
-			p0Vec.add(Math.log((double) (p0Num[m]) / p0Den));
+			for (int m = 0; m < wordsNum; m++) {
+				p1Vec.add(Math.log((double) (p1Num[m]) / p1Den));
+				p0Vec.add(Math.log((double) (p0Num[m]) / p0Den));
+			}
 		}
 		System.out.println("Training complete!");
 	}
 
+	//将字典和特征向量写入文件，下次直接读入
+	private static void writeDictnFeature() {
+		if(hasHistory){
+			//有历史则不再写入dict.txt
+		}
+		else {
+			//将字典写入历史
+			FileOutputStream fos = null;
+			try {
+				fos = new FileOutputStream("./dict.txt");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			BufferedWriter bw = new BufferedWriter(osw);
+			for (int i = 0; i < vocabulary.size(); i++) {
+				try {
+					bw.write(vocabulary.get(i));
+					bw.write("\r\n");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			try {
+				bw.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			//将特征向量p0写入历史
+			try {
+				fos = new FileOutputStream("./p0Vec.txt");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			osw = new OutputStreamWriter(fos);
+			bw = new BufferedWriter(osw);
+			try {
+				for(int i = 0;i<p0Vec.size();i++) {
+					bw.write(p0Vec.get(i)+"\t");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				bw.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			//将特征向量p1写入历史
+			try {
+				fos = new FileOutputStream("./p1Vec.txt");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			osw = new OutputStreamWriter(fos);
+			bw = new BufferedWriter(osw);
+			try {
+				for(int i = 0;i<p1Vec.size();i++) {
+					bw.write(p1Vec.get(i)+"\t");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				bw.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			//将特征概率pAbusive写入历史
+			try {
+				fos = new FileOutputStream("./pAbusive.txt");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			osw = new OutputStreamWriter(fos);
+			bw = new BufferedWriter(osw);
+			try {
+				bw.write(Double.toString(pAbusive));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				bw.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	//判决
 	public static void judge(List<Integer> testVec) {
 
 		double p1 = 0;
@@ -211,7 +384,7 @@ public class Bayesian {
 		}
 		p1 += Math.log(pAbusive);
 		p0 += Math.log(1 - pAbusive);
-		System.out.println("p0=" + p0 + "   p1=" + p1);
+		System.out.println("p0=" + p0 + "   p1=" + p1 + "   pA="+pAbusive);
 		if(p0 > -0.4 || p1>-1.4){
 			System.out.println("Neutral Words!");
 		}
@@ -231,21 +404,20 @@ public class Bayesian {
 		return s;
 	}
 
-	public static List<Integer> test2Vector(List<String> vocab, List<String> data) {
+	public static List<Integer> test2Vector(List<String> data) {
 		List<Integer> vec = new ArrayList<>();
-		int[] temp = new int[vocab.size()];
-		for (int i = 0; i < vocab.size(); i++) {
+		int[] temp = new int[vocabulary.size()];
+		for (int i = 0; i < vocabulary.size(); i++) {
 			vec.add(0);
 		}
 		for (int i = 0; i < data.size(); i++) {
-			if (vocab.contains(data.get(i))) {
-				temp[(vocab.indexOf(data.get(i)))]++;
+			if (vocabulary.contains(data.get(i))) {
+				temp[(vocabulary.indexOf(data.get(i)))]++;
 			}
 		}
-		for (int i = 0; i < vocab.size(); i++) {
+		for (int i = 0; i < vocabulary.size(); i++) {
 			vec.set(i, temp[i]);
 		}
-		System.out.println(vec);
 		return vec;
 	}
 }
